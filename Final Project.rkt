a1294df01f711b0e14bebabde3038c405e8ad993
Josiah Pang <pang.josiah@gmail.com>
2016-11-13 20:54:57 -0800

    Added comments

--- Final Project.rkt
+++ Final Project.rkt
@@ -13,6 +13,7 @@
 (define INITIAL-STATE
   (make-ws "0"))
 
+;; Defines constants
 (define FR-RATE 44100)
 (define PR-WIDTH 1400)
 (define PR-HEIGHT 800)
@@ -87,7 +88,7 @@
                550 500
                BG))))))))))
 
-
+;; Figures out which key was pressed using x/y coordinates
 (define (checkKey x y)
   (cond [(and (>= x 1100) (< x 1250))
                (cond [(and (>= y 200) (< y 350)) sqKey1]
@@ -103,8 +104,8 @@
                         [else blankKey])]
          [else blankKey]))
 
-; PSTREAM TEST STUFF
-(define rstream (make-pstream))
+;; Plays drum noises when key is clicked
+(define rstream (make-pstream)) ;; Defines pstream
 (define (playKey key)
   (cond [(equal? key sqKey1) (pstream-play rstream kick)]
         [(equal? key sqKey2) (pstream-play rstream bassdrum)]
@@ -114,20 +115,9 @@
         [(equal? key sqKey6) (pstream-play rstream crash-cymbal)]
         [(equal? key sqKey7) (pstream-play rstream snare)]
         [(equal? key sqKey8) (pstream-play rstream ding)]
-        [else (pstream-play rstream (silence 1))]))
+        [else (pstream-play rstream (silence 1))])) ;; Silence if nothing is pressed
 
-; ACTUAL FUNCTION
-#;(define (playKey key)
-  (cond [(equal? key sqKey1) (play kick)]
-        [(equal? key sqKey2) (play bassdrum)]
-        [(equal? key sqKey3) (play o-hi-hat)]
-        [(equal? key sqKey4) (play c-hi-hat-1)]
-        [(equal? key sqKey5) (play clap-1)]
-        [(equal? key sqKey6) (play crash-cymbal)]
-        [(equal? key sqKey7) (play snare)]
-        [(equal? key sqKey8) (play ding)]
-        [else (play (silence 1))]))
-
+;; Defines mouse handler
 (define (handle-mouse ws x y event)
   (cond [(string=? event "button-down")
          (both (playKey (checkKey x y))
@@ -136,6 +126,7 @@
 
 (define (both a b) b)
 
+;; Creates big bang
 (big-bang INITIAL-STATE
           [to-draw draw-keys]
           [on-mouse handle-mouse])
