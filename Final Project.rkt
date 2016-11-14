b78e7df0c58172b1540030f61de1073696b41939
Celestine Co <celestine.l.co@intel.com>
2016-11-13 20:15:14 -0800

    Finished drum keys.

--- Final Project.rkt
+++ Final Project.rkt
@@ -13,6 +13,7 @@
 (define INITIAL-STATE
   (make-ws "0"))
 
+(define FR-RATE 44100)
 (define PR-WIDTH 1400)
 (define PR-HEIGHT 800)
 (define SQ-KEY-SIZE 150)
@@ -54,42 +55,74 @@
                           SQ-KEY-SIZE
                           "solid"
                           "Linen"))
-
-(define sqKeyList
-  (list
-   sqKey1
-   sqKey2
-   sqKey3 
-   sqKey4
-   sqKey5
-   sqKey6
-   sqKey7
-   sqKey8))
+(define blankKey (rectangle SQ-KEY-SIZE
+                            SQ-KEY-SIZE
+                            "solid"
+                            "white"))
+;make structure for piano keys, so when piano key is called you can just play certain synth-note
 
 ;; piano = 1100x600
-(define piano (rectangle 1100 600 "solid" "white"))
+;(define piano (rectangle 1100 600 "solid" "white"))
+(define piano (bitmap "piano.png"))
 
 ;; Image will be placed with midpoint as reference
 (define (draw-keys ws)
-  (place-image sqKey1
-               1175 725
-  (place-image sqKey2
-               1325 725
-  (place-image sqKey3
-               1175 575
-  (place-image sqKey4
-               1325 575
-  (place-image sqKey5
-               1175 425
-  (place-image sqKey6
-               1325 425
   (place-image sqKey7
-               1175 275
+               1175 725
   (place-image sqKey8
+               1325 725
+  (place-image sqKey5
+               1175 575
+  (place-image sqKey6
+               1325 575
+  (place-image sqKey3
+               1175 425
+  (place-image sqKey4
+               1325 425
+  (place-image sqKey1
+               1175 275
+  (place-image sqKey2
                1325 275
   (place-image piano
                550 500
                BG))))))))))
 
+
+(define (checkKey x y)
+  (cond [(and (>= x 1100) (< x 1250))
+               (cond [(and (>= y 200) (< y 350)) sqKey1]
+                     [(and (>= y 350) (< y 500)) sqKey3]
+                     [(and (>= y 500) (< y 650)) sqKey5]
+                     [(and (>= y 650) (<= y 800)) sqKey7]
+                     [else blankKey])]
+         [(and (>= x 1250) (<= x 1400))
+                  (cond [(and (>= y 200) (< y 350)) sqKey2]
+                        [(and (>= y 350) (< y 500)) sqKey4]
+                        [(and (>= y 500) (< y 650)) sqKey6]
+                        [(and (>= y 650) (<= y 800)) sqKey8]
+                        [else blankKey])]
+         [else blankKey]))
+
+
+(define (playKey key)
+  (cond [(equal? key sqKey1) (play kick)]
+        [(equal? key sqKey2) (play bassdrum)]
+        [(equal? key sqKey3) (play o-hi-hat)]
+        [(equal? key sqKey4) (play c-hi-hat-1)]
+        [(equal? key sqKey5) (play clap-1)]
+        [(equal? key sqKey6) (play crash-cymbal)]
+        [(equal? key sqKey7) (play snare)]
+        [(equal? key sqKey8) (play ding)]
+        [else (play (silence 1))]))
+
+(define (handle-mouse ws x y event)
+  (cond [(string=? event "button-down")
+         (both (playKey (checkKey x y))
+               ws)]
+        [else ws]))
+
+(define (both a b) b)
+
 (big-bang INITIAL-STATE
-          [to-draw draw-keys])
+          [to-draw draw-keys]
+          [on-mouse handle-mouse])
